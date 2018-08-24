29c9ef6d2570a52599e1b5e774432364d00aa33e
Ivan <icnovaro@gmail.com>
2018-08-23 20:44:20 -0500

    adds the Recipe's model

---
+++ app/models/Recipe.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class Recipe {
+
+    public Integer id;
+    public String name;
+    public String description;
+
+    public Recipe(Integer id, String name, String description){
+        this.id=id;
+        this.name=name;
+        this.description=description;
+    }
+
+    private static Set<Recipe> recipes;
+
+    static{
+        recipes = new HashSet<>();
+        recipes.add(new Recipe(1, "Fried eggs", "put the eggs and wait until they get fried"));
+        recipes.add(new Recipe(2, "Rice", "Put rice with water and wait until it boil"));
+    }
+
+    public static Set<Recipe> getAll(){
+        return recipes;
+    }
+
+    public static Recipe getById(Integer id){
+        for(Recipe recipe: recipes){
+            if(id.equals(recipe.id)){
+                return recipe;
+            }
+        }
+        return null;
+    }
+
+    public static void add(Recipe recipe){
+        recipes.add(recipe);
+    }
+
+    public static boolean remove(Recipe recipe){
+        return recipes.remove(recipe);
+    }
+}
