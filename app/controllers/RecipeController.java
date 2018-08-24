d77084857699c627f6e4b69a3cd2b1e6a408bb0c
Ivan <icnovaro@gmail.com>
2018-08-23 20:24:39 -0500

    creates routes and controller's methods for Recipes

--- app/controllers/RecipeController.java
+++ app/controllers/RecipeController.java
@@ -6,7 +6,35 @@ import views.html.recipes.*;
 
 public class RecipeController extends Controller {
 
-    public Result retrieve(String id) {
-        return ok(recipe.render(id));
+    public Result index(){
+        return TODO;
     }
+
+    public Result retrieve(Integer id) {
+        return ok(recipe.render("hola"));
+    }
+
+    public Result create(){
+        return TODO;
+    }
+
+    //GET edit
+    public Result edit(Integer id){
+        return TODO;
+    }
+
+    //POST edit
+    public Result update(){
+        return TODO;
+    }
+
+
+    public Result save(){
+        return TODO;
+    }
+
+    public Result destroy(Integer id){
+        return TODO;
+    }
+
 }
